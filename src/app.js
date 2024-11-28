7a9711e5baf0e7cae0cac28c97f4fc3f151f039c
Ruben <167398241+rumacon@users.noreply.github.com>
2024-11-28 02:47:21 +0000

    al fin

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let nombre = ["perro", "loro", "toro"];
+let cualidad = ["loco", "tuerto", "cojo"];
+let apellido = ["McCoy", "Trump", "Jackson"];
+let domain = [".net", ".com", ".mx"];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  for (let name = 0; name <= 2; name++) {
+    for (let qual = 0; qual <= 2; qual++) {
+      for (let last = 0; last <= 2; last++) {
+        for (let dom = 0; dom <= 2; dom++) {
+          console.log(
+            nombre[name] + cualidad[qual] + apellido[last] + domain[dom]
+          );
+        }
+      }
+    }
+  }
 };
