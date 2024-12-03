c33548f5b606685d817a1e758a0f25f24f75c609
Ruben <167398241+rumacon@users.noreply.github.com>
2024-12-03 01:11:12 +0000

    domaine

--- src/app.js
+++ src/app.js
@@ -1,21 +1,27 @@
 /* eslint-disable */
+/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let nombre = ["perro", "loro", "toro"];
+let cualidad = ["loco", "tuerto", "cojo"];
+let apellido = ["McCoy", "Trump", "Jackson"];
+let domain = [".net", ".com", ".mx"];
+
 window.onload = function() {
-  let nombre = ["mi perro", "mi amigo", "mi hijo", "mi compadre"];
-  let acto = ["me pego", "me robo", "me pateo", "me escupio"];
-  let cosa = ["el zapato", "la cartera", "la mesa", "la silla"];
-  let lugar = ["en la calle", "en la banqueta", "en Mexico", "en la esquina"];
-
-  let ran1 = Math.floor(Math.random() * nombre.length);
-  let ran2 = Math.floor(Math.random() * acto.length);
-  let ran3 = Math.floor(Math.random() * cosa.length);
-  let ran4 = Math.floor(Math.random() * lugar.length);
-
-  document.querySelector("#excuse").innerHTML =
-    nombre[ran1] + " " + acto[ran2] + " " + cosa[ran3] + " " + lugar[ran4];
+  for (let name = 0; name <= 2; name++) {
+    for (let qual = 0; qual <= 2; qual++) {
+      for (let last = 0; last <= 2; last++) {
+        for (let dom = 0; dom <= 2; dom++) {
+          console.log(
+            nombre[name] + cualidad[qual] + apellido[last] + domain[dom]
+          );
+        }
+      }
+    }
+  }
 };
+
